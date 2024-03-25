eaad85157c3f5c0244be907beab0611effe43cc3
Bronius <bronius256@gmail.com>
2024-03-25 17:44:18 +0200

    DTO created

--- src/main/java/local/controllers/GameController.java
+++ src/main/java/local/controllers/GameController.java
@@ -1,7 +1,7 @@
 package local.controllers;
 
 import local.services.GameManagerService;
-import local.MoveRequest;
+import local.dto.MoveRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- src/main/java/local/dto/MoveRequest.java
+++ src/main/java/local/dto/MoveRequest.java
@@ -1,4 +1,4 @@
-package local;
+package local.dto;
 
 public class MoveRequest {
     private int tileValue;
