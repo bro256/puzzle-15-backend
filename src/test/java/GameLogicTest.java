58063699e0a804ff567d31fce95aa9d17f684f82
Bronius <bronius256@gmail.com>
2024-03-25 17:39:04 +0200

    Test fix

--- src/test/java/GameLogicTest.java
+++ src/test/java/GameLogicTest.java
@@ -34,35 +34,5 @@ public class GameLogicTest {
         }
     }
 
-    @Test
-    public void testGameComplete() {
-        int[][] gameState = {
-                {1, 2, 3, 4},
-                {5, 6, 7, 8},
-                {9, 10, 11, 12},
-                {13, 14, 15, 0}
-        };
-
-        assertTrue(isGameComplete(gameState));
-    }
-    private boolean isGameComplete(int[][] gameState) {
-        int expectedValue = 1;
-        int totalTiles = gameState.length * gameState[0].length;
-
-        for (int[] row : gameState) {
-            for (int value : row) {
-                if (value != expectedValue) {
-                    // Check last value
-                    boolean isLastExpectedValue = (expectedValue == totalTiles && value == 0);
-                    if (!isLastExpectedValue) {
-                        return false;
-                    }
-                }
-                expectedValue++;
-            }
-        }
-        return true;
-    }
-
 
 }
